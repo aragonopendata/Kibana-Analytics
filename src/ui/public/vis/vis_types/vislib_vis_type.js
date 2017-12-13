7b8c290d0a2e7f1699ca8915cdeced2fff05a6a8
Julio <julio88.99@gmail.com>
2017-12-13 15:37:27 +0100

    Default Click Event Deleted

--- src/ui/public/vis/vis_types/vislib_vis_type.js
+++ src/ui/public/vis/vis_types/vislib_vis_type.js
@@ -31,7 +31,7 @@ export function VislibVisTypeProvider(Private) {
         if (!this._response) return reject();
         this.vis.vislibVis = new vislib.Vis(this.el, this.vis.params);
         this.vis.vislibVis.on('brush', this.vis.API.events.brush);
-        this.vis.vislibVis.on('click', this.vis.API.events.filter);
+        //this.vis.vislibVis.on('click', this.vis.API.events.filter);
         this.vis.vislibVis.on('renderComplete', resolve);
         this.vis.vislibVis.render(esResponse, this.vis.getUiState());
       });
