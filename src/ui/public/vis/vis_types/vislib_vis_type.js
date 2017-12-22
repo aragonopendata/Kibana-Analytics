569d44d8168b5de8b90db8f5402757426cc5a508
Julio <julio88.99@gmail.com>
2017-12-22 09:48:56 +0100

    default brush event deleted

--- src/ui/public/vis/vis_types/vislib_vis_type.js
+++ src/ui/public/vis/vis_types/vislib_vis_type.js
@@ -30,7 +30,7 @@ export function VislibVisTypeProvider(Private) {
       return new Promise((resolve, reject) => {
         if (!this._response) return reject();
         this.vis.vislibVis = new vislib.Vis(this.el, this.vis.params);
-        this.vis.vislibVis.on('brush', this.vis.API.events.brush);
+        //this.vis.vislibVis.on('brush', this.vis.API.events.brush);
         //this.vis.vislibVis.on('click', this.vis.API.events.filter);
         this.vis.vislibVis.on('renderComplete', resolve);
         this.vis.vislibVis.render(esResponse, this.vis.getUiState());
